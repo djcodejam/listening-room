29276a4fbccc596a3e27415fbcb327b967fdd131
Christopher Acosta <chrisacosta12345@gmail.com>
2023-11-13 15:23:55 -0800

    fixed the video rendering

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 console.log('words');
 const videoContainer = document.getElementById('videos');
 const vid1 = document.querySelector('#videos video:first-child');
-const vid2 = document.querySelector('#videos video:second-child');
+const vid2 = document.querySelector('#videos video:nth-child(2)');
 
 let state = {
   clicksSoFar: 0,
@@ -18,9 +18,11 @@ function BPMvids(name, link, type){
   state.allBPMvids.push(this);
 }
 
-new BPMvids('test-vid1', 'https://www.shutterstock.com/video/search/anger', 'mp4');
-new BPMvids('test-vid2', 'test2', 'song2');
-new BPMvids('test-vid3', 'test3', 'song3');
+new BPMvids('test-vid1', 'videos/test.mp4', 'mp4');
+new BPMvids('test-vid2', 'videos/test.mp4', 'mp4');
+
+// new BPMvids('test-vid2', 'test2', 'song2');
+// new BPMvids('test-vid3', 'test3', 'song3');
 
 function renderBpmVideos(){
 
@@ -48,25 +50,25 @@ function GenreVids(name, link, type){
   this.type = type;
 }
 
-new GenreVids('testGenre');
+new GenreVids('testGenre', 'testLink');
 
-function renderGenreVideos(){
-  function randomVid(){
-    return Math.floor(Math.random() * state.allGenrevids.length);
-  }
+// function renderGenreVideos(){
+//   function randomVid(){
+//     return Math.floor(Math.random() * state.allGenrevids.length);
+//   }
 
-  let video1 = randomVid();
-  let video2 = randomVid();
+//   let video1 = randomVid();
+//   let video2 = randomVid();
 
-  while (video2 === video1){
-    video2 = randomVid();
-  }
-  vid1.src = state.allGenrevids[video1].link;
-  vid1.type = state.allGenrevids[video1].type;
+//   while (video2 === video1){
+//     video2 = randomVid();
+//   }
+//   vid1.src = state.allGenrevids[video1].link;
+//   vid1.type = state.allGenrevids[video1].type;
 
-  vid2.src = state.allGenrevids[video2].link;
-  vid2.type = state.allGenrevids[video2].type;
-}
+//   vid2.src = state.allGenrevids[video2].link;
+//   vid2.type = state.allGenrevids[video2].type;
+// }
 
 function eventListeners(){
   videoContainer.addEventListener('click', handleClick );
@@ -77,9 +79,9 @@ function handleClick(event){
 
   state.clicksSoFar++;
 
-  if(state.clicksSoFar >= 3){
-    renderGenreVideos();
-  }
+  // if(state.clicksSoFar >= 3){
+  //   renderGenreVideos();
+  // }
   if(state.clicksSoFar >= state.clicksAllowed){
     removeEventListener();
   }
